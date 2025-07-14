3d53456c87f99fc5b147876634d1a53dd9d1e7ee
Jumani Clarke <jumani.clarke@uct.ac.za>
2025-07-14 14:07:53 +0200

    Update methods.py

--- methods.py
+++ methods.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import psycopg2-binary as ps
+import psycopg2-binary as psy
 import psycopg2.extras as extras
 
 
@@ -11,7 +11,7 @@ cur = None
 
 @st.cache_resource
 def init_connection():
-    return ps.connect(**st.secrets["postgres"])
+    return psy.connect(**st.secrets["postgres"])
 # make a connection to the database
 conn1 = init_connection()
 
